cbf750eb56607d5635e96962137f0cb320866660
Itsmehari <harikrishnanhk1988@gmail.com>
2025-04-18 15:24:04 +0530

    Update ExploreMenu.jsx

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from 'react';
-import { foodList } from '../../data/menuData';
-import * as assets from '../../assets/assets';
+import React from 'react';
+import './ExploreMenu.css';
+import { assets } from '../../assets/assets';
 
+const categories = [
+  { name: 'Appetizers', image: assets.menu_1 },
+  { name: 'Bread Rolls', image: assets.menu_2 },
+  { name: 'Bread', image: assets.menu_3 },
+  { name: 'Burritos', image: assets.menu_4 },
+  { name: 'Curry', image: assets.menu_5 },
+  { name: 'Dessert', image: assets.menu_6 },
+  { name: 'Rice Bowl', image: assets.menu_7 },
+  { name: 'Rice Variety', image: assets.menu_8 }
+];
 
-function ExploreMenu() {
-  const categories = [...new Set(foodList.map(item => item.category || 'All'))];
-  const [category, setCategory] = useState('All');
-
-  const filteredList = category === 'All' ? foodList : foodList.filter(item => item.category === category);
-
+const ExploreMenu = () => {
   return (
-    <div className="explore-menu">
-      <div className="categories">
-        {categories.map((cat, index) => (
-          <button key={index} className={cat === category ? 'active' : ''} onClick={() => setCategory(cat)}>
-            {cat}
-          </button>
-        ))}
-      </div>
-      <div className="menu-list">
-        {filteredList.map(item => (
-          <div key={item.id}>
-            <img src={assets[item.image]} alt={item.name} />
-            <p>{item.name} - ${item.price}</p>
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring a delectable array of dishes.
+        Our mission is to satisfy your cravings and elevate your dining experience,
+        one delicious meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {categories.map((item, index) => (
+          <div key={index} className="explore-menu-list-item">
+            <img src={item.image} alt={item.name} />
+            <p>{item.name}</p>
           </div>
         ))}
       </div>
+      <hr />
     </div>
   );
-}
+};
 
 export default ExploreMenu;
