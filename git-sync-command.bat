ca627c88be54badc9ad3979d509fbe6b11862302
unknown <harikrishnanhk1988@gmail.com>
2025-04-27 09:49:33 +0530

    First commit after renaming to main

--- git-sync-command.bat
+++ git-sync-command.bat
@@ -1,7 +1,19 @@
 @echo off
 cd /d "C:\Users\Admin\OneDrive\Aaryaas\Project-Back-Ups\Aryas-Fooddelivery-main-18042025-v1-orginal\Fooddelivery-main"
 git init
-git add .
-git commit -m "Auto Sync"
-git push origin main
+:: Fetch latest status
+git status
+
+:: Check if there are any changes
+git diff-index --quiet HEAD --
+if %errorlevel% neq 0 (
+    git add .
+    set /p commitMessage="Enter Commit Message: "
+    git commit -m "%commitMessage%"
+    git push origin main
+    echo Changes pushed successfully!
+) else (
+    echo No changes detected. Nothing to commit.
+)
+
 pause
