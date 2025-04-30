2a5d6500ee7db27653a6cb510d91e2264bec46f4
unknown <harikrishnanhk1988@gmail.com>
2025-04-30 08:40:32 +0530

    Fixed merge conflict in Footer.jsx and improved branding consistency

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import './Footer.css'
 import { assets } from '../../assets/assets'
@@ -8,50 +7,8 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
-        </div>
-        <div className="footer-content-center">
-            <h2>COMPANY</h2>
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
-            </ul>
-        </div>
-        <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@aryascusine.com</li>
-            </ul>
-        </div>
-      </div>
-      <hr />
-      <p className="footer-copyright">Copyright 2024 © Arayscusine.com - All Right Reserved.</p>
-    </div>
-  )
-}
-
-export default Footer
-=======
-import React from 'react'
-import './Footer.css'
-import assets from '../../assets/assets'
-
-const Footer = () => {
-  return (
-    <div className='footer' id='footer'>
-      <div className="footer-content">
-        <div className="footer-content-left">
-          <img src={assets.logo} alt="Logo" />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero voluptates, cum officiis nesciunt iusto repellendus consectetur.</p>
+          <img src={assets.logo} alt="Aryas Cuisine Logo" />
+          <p>Experience the authentic taste of traditional cuisine with a modern twist. Our commitment to quality ingredients and exceptional service makes every meal special.</p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="Facebook" />
             <img src={assets.twitter_icon} alt="Twitter" />
@@ -70,18 +27,17 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+91 123 456 789</li>
-            <li>contact@tastygo.com</li>
+            <li>+1-212-456-7890</li>
+            <li>contact@aryascuisine.com</li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        © 2025 TastyGo. All Rights Reserved.
+        Copyright 2024 © Aryascuisine.com - All Rights Reserved.
       </p>
     </div>
   )
 }
 
 export default Footer
->>>>>>> 654e5f637d09ea247cc9544d678011ed88cca846
